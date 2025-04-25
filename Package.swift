4f3cc01966cf3ef09676c2e9b914cb53c5ba88bf
arbigaus <gerson87@gmail.com>
2025-04-24 21:34:43 -0300

    Test chante macos version in package

--- Package.swift
+++ Package.swift
@@ -1,25 +1,21 @@
 // swift-tools-version: 6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-import PackageDescription
 import CompilerPluginSupport
+import PackageDescription
 
 let package = Package(
     name: "GAMacros",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+    platforms: [.macOS(.v12), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "GAMacros",
             targets: ["GAMacros"]
-        ),
-        .executable(
-            name: "GAMacrosClient",
-            targets: ["GAMacrosClient"]
-        ),
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0-latest"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0-latest")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -29,19 +25,12 @@ let package = Package(
             name: "GAMacrosMacros",
             dependencies: [
                 .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
-                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
-            ],
-            path: "Sources/GAMacrosMacros",
-            swiftSettings: [
-                .define("ENABLE_MACRO_PLUGIN")
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
             ]
         ),
 
         // Library that exposes a macro as part of its API, which is used in client programs.
         .target(name: "GAMacros", dependencies: ["GAMacrosMacros"]),
-
-        // A client of the library, which is able to use the macro in its own code.
-        .executableTarget(name: "GAMacrosClient", dependencies: ["GAMacros"]),
 
         // A test target used to develop the macro implementation.
         .testTarget(
